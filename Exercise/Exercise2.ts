1078f2e15b50490b664baa764d6ed174d14231b3
GuyenTu8765 <tunguyen.080701@gmail.com>
2022-10-19 01:58:22 +0900

    add new

--- Exercise/Exercise2.ts
+++ Exercise/Exercise2.ts
@@ -1,61 +1,53 @@
 export function question1() {
+  var fullName: string = "Nguyen Van Tu";
+  var age: number = 21;
 
-    var fullName: string = "Nguyen Van Tu";
-    var age: number = 21;
-
-    var content = `Tôi tên là ${fullName} năm nay ${age} tuổi, đang ế!`
-    console.log(content);
-
+  var content = `Tôi tên là ${fullName} năm nay ${age} tuổi, đang ế!`;
+  console.log(content);
 }
 
 export function question2() {
+  var a: number = 5;
+  var b: number = 20;
 
-    var a: number = 5;
-    var b: number = 20;
-
-    var content = `Tổng của ${a} và ${b} là ${a + b}`;
-    console.log(content);
-
+  var content = ` Tổng của ${a} và ${b} là ${a + b} `;
+  console.log(content);
 }
 
 export function question3() {
-    var food = [
-        {
-            food: "1. Chả giò nón lá"
-        },
-        {
-            food: "2. Bò lúc lắc hạt điều"
-        },
-        {
-            food: "3. Cá tai tượng chiên xù"
-        },
-        {
-            food: "4. Cải thìa sốt thịt cua"
-        },
-        {
-            food: "5. Cơm chiên cá mặn"
-        },
-        {
-            food: "6. Lẩu hải sản"
-        },
-        {
-            food: "7. Rau câu"
-        }
-    ];
+  var food = [
+    {
+      food: "1. Chả giò nón lá",
+    },
+    {
+      food: "2. Bò lúc lắc hạt điều",
+    },
+    {
+      food: "3. Cá tai tượng chiên xù",
+    },
+    {
+      food: "4. Cải thìa sốt thịt cua",
+    },
+    {
+      food: "5. Cơm chiên cá mặn",
+    },
+    {
+      food: "6. Lẩu hải sản",
+    },
+    {
+      food: "7. Rau câu",
+    },
+  ];
 
-    food.map(function (food, key) {
-        var result = `
+  food.map(function (food, key) {
+    var result = `
         <div>
             <h1>${food.food}</h1>
         </div>
         `;
-        
-        console.log(result);
 
-    });
+    console.log(result);
+  });
 }
 
-
-
-export default { question1, question2, question3 }
-
+export default { question1, question2, question3 };
--- Exercise/Exercise5.ts
+++ Exercise/Exercise5.ts
@@ -1,44 +1,34 @@
 export function question1() {
+  console.log("Que1----------------------------");
+  var multiply = (a, b: number = 1) => console.log(a * b);
 
-    console.log("Que1----------------------------");
-    function multiply(a, b: number = 1) {
-        console.log(a * b);
-    }
-    multiply(5, 3);
+  multiply(5, 3);
+  multiply(5);
 }
 
 export function question2() {
-
-    console.log("Que2----------------------------");
-    var multiply = (...numParam) => {
-        var result = 1;
-        numParam.forEach(e => {
-            result * -e;
-        });
-        // numParam.map(function (item, key) {
-        //     result * item;
-        // })
-        console.log(result);
-    }
-
-    multiply(5 );
-
+  console.log("Que2----------------------------");
+  var multiply = (...numParam) => {
+    var result = 1;
+    numParam.forEach((e) => {
+      result *-e;
+    });
+    console.log(result);
+  };
+  multiply(5);
 }
 export function question3() {
-
-    console.log("Que3----------------------------");
-    var printInformation = (name: string, email?: string) => {
-        if (email != undefined) {
-            var content = `Tôi là ${name}. Email của tôi là ${email}`;
-            console.log(content);
-        } else {
-            var content = `Tôi là ${name}. Tôi chưa có Email`;
-            console.log(content);
-        }
+  console.log("Que3----------------------------");
+  var printInformation = (name: string, email?: string) => {
+    if (email != undefined) {
+      var content = `Tôi là ${name}. Email của tôi là ${email}`;
+      console.log(content);
+    } else {
+      var content = `Tôi là ${name}. Tôi chưa có Email`;
+      console.log(content);
     }
-    printInformation("Name", "Namvv20@gmail.com");
+  };
+  printInformation("Name", "Namvv20@gmail.com");
 }
 
-
-
-export default { question1, question2, question3 }
+export default { question1, question2, question3 };
